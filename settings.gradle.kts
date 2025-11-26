2db16603414a603f0a35d662c476ec2044d77db1
roman.c-e <rce@zivue.com>
2025-11-26 14:16:51 +0100

    rm db dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,8 +22,6 @@ dependencyResolutionManagement {
             version("jsch", "0.2.16")
             version("cxf", "3.6.6")
             version("hoplite", "2.8.2")
-            version("sqldelight", "2.0.2")
-            version("postgres", "42.7.4")
             version("vault-jdbc", "1.3.10")
             version("emottak-utils", "0.3.1")
 
@@ -62,20 +60,10 @@ dependencyResolutionManagement {
             library("logback-classic", "ch.qos.logback:logback-classic:1.5.18")
             library("logback-logstash", "net.logstash.logback:logstash-logback-encoder:8.1")
 
-            library("sqldelight-jdbc-driver", "app.cash.sqldelight", "jdbc-driver").versionRef("sqldelight")
-            library("sqldelight-postgresql-dialect", "app.cash.sqldelight", "postgresql-dialect").versionRef("sqldelight")
-            library("sqldelight-coroutines-extensions", "app.cash.sqldelight", "coroutines-extensions").versionRef("sqldelight")
-            library("sqldelight-primitive-adapters", "app.cash.sqldelight", "primitive-adapters").versionRef("sqldelight")
-
-            library("postgresql", "org.postgresql", "postgresql").versionRef("postgres")
-
-            library("vault-jdbc", "no.nav", "vault-jdbc").versionRef("vault-jdbc")
-
             library("ebxml-protokoll", "no.nav.emottak:ebxml-protokoll:0.0.6")
             library("ebxml-processing-model", "no.nav.emottak:ebxml-processing-model:2024102305ad1b37a7c4ec36bc77ef91f5ff67098389922f")
             library("emottak-payload-xsd", "no.nav.emottak:emottak-payload-xsd:0.0.9")
             library("emottak-utils", "no.nav.emottak", "emottak-utils").versionRef("emottak-utils")
-            library("hikari", "com.zaxxer:HikariCP:5.0.1")
             library("labai-jsr305x-annotations", "com.github.labai:labai-jsr305x-annotations:0.0.2")
             library("jakarta.xml.bind-api", "jakarta.xml.bind:jakarta.xml.bind-api:4.0.0")
             library("ktor-server-auth-jvm", "io.ktor", "ktor-server-auth-jvm").versionRef("ktor")
@@ -102,13 +90,10 @@ dependencyResolutionManagement {
             version("junit", "5.9.2")
             version("hamcrest", "2.2")
             version("mockk", "1.13.10")
-            version("testPostgres", "1.18.0")
             version("xmlunit", "2.9.1")
             version("ktor-server-test", "3.0.3")
             version("kotest", "5.9.1")
-            version("testcontainers", "1.18.1")
             version("kotest-extensions", "2.0.2")
-            version("postgres", "42.7.4")
             version("turbine", "1.2.0")
             version("arrow", "2.0.1")
 
@@ -131,11 +116,6 @@ dependencyResolutionManagement {
             library("kotest-extensions-testcontainers", "io.kotest.extensions", "kotest-extensions-testcontainers").versionRef("kotest-extensions")
 
             library("kotest-assertions-arrow", "io.kotest.extensions", "kotest-assertions-arrow").versionRef("arrow")
-
-            library("testcontainers", "org.testcontainers", "testcontainers").versionRef("testcontainers")
-            library("testcontainers-postgresql", "org.testcontainers", "postgresql").versionRef("testcontainers")
-
-            library("postgresql", "org.postgresql", "postgresql").versionRef("postgres")
 
             library("turbine", "app.cash.turbine", "turbine").versionRef("turbine")
 
