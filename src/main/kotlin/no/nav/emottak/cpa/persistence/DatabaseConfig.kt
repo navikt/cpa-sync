090b6e478b53e0706ce526c27aca79d6c7f74931
Aage Kramarics <Aage.Kramarics@nav.no>
2026-02-24 08:00:34 +0100

    Prøver å få lokal tidssone for CURRENT TIMESTAMP

--- src/main/kotlin/no/nav/emottak/cpa/persistence/DatabaseConfig.kt
+++ src/main/kotlin/no/nav/emottak/cpa/persistence/DatabaseConfig.kt
@@ -2,6 +2,7 @@ package no.nav.emottak.cpa.persistence
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import kotlinx.datetime.TimeZone
 import org.jetbrains.exposed.sql.Database
 import org.slf4j.LoggerFactory
 import java.nio.file.Files
@@ -16,16 +17,20 @@ data class DatabaseConfig(
 )
 
 fun configureCpaArchiveRepository(databaseConfig: DatabaseConfig): CpaArchiveRepository {
+    // Når vi bruker CURRENT TIMESTAMP i inserts, får vi GMT-tid. Prøver å fikse det med denne.
+    val ourTimezone = TimeZone.currentSystemDefault().id
     val hikariConfig = HikariConfig().apply {
         jdbcUrl = databaseConfig.jdbcUrl
         driverClassName = "oracle.jdbc.OracleDriver"
         maximumPoolSize = databaseConfig.maxPoolSize
         username = readFromFile(databaseConfig.secretPath + "/username")
         password = readFromFile(databaseConfig.secretPath + "/password")
+        connectionInitSql = "ALTER SESSION SET TIME_ZONE = '$ourTimezone'"
     }
-    log.info("DB URL set to {}, with user {}", hikariConfig.jdbcUrl, hikariConfig.username)
+    log.info("DB URL set to {}, with user {}, init-sql: {}", hikariConfig.jdbcUrl, hikariConfig.username, hikariConfig.connectionInitSql)
     val dataSource = HikariDataSource(hikariConfig)
     val database = Database.connect(dataSource)
+
     return CpaArchiveRepository(database)
 }
 
